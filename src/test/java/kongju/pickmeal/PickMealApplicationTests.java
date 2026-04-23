4165d31fd85de03fde8546bd36b9f78fd06358f6
jihyun <rlawlgus4980@gmail.com>
2026-04-23 17:08:38 +0900

    feat: 프로젝트 초기 환경 설정

---
+++ src/test/java/kongju/pickmeal/PickMealApplicationTests.java
@@ -0,0 +1,13 @@
+package kongju.pickmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickMealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
