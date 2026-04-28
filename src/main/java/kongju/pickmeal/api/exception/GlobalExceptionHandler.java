b2f87914e3c126a4a64a311a768c121993dadefe
jihyun <rlawlgus4980@gmail.com>
2026-04-28 18:21:36 +0900

    test : Valid에러 핸들러 추가

--- src/main/java/kongju/pickmeal/api/exception/GlobalExceptionHandler.java
+++ src/main/java/kongju/pickmeal/api/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package kongju.pickmeal.api.exception;
 import kongju.pickmeal.common.ApiResponse.ApiResponse;
 import kongju.pickmeal.common.exception.BusinessException;
 import kongju.pickmeal.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +24,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ApiResponse.fail(errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e){
+        String detailMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.fail(ErrorCode.INVALID_INPUT, detailMessage));
     }
 }
