03e688768ebb49c4c65e3b19ac6eb1879b02ada9
jihyun <rlawlgus4980@gmail.com>
2026-05-04 17:33:02 +0900

    feat : 블랙리스트 추가

--- src/main/java/kongju/pickmeal/api/security/JwtTokenFilter.java
+++ src/main/java/kongju/pickmeal/api/security/JwtTokenFilter.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.List;
 
 import jakarta.servlet.FilterChain;
+import kongju.pickmeal.common.exception.BusinessException;
+import kongju.pickmeal.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import kongju.pickmeal.core.service.JwtService;
 import jakarta.servlet.http.HttpServletResponse;
 import kongju.pickmeal.core.user.UserRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +24,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 public class JwtTokenFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -37,6 +41,12 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         }
         // Bearer뒷부분 토큰값 추출
         String token = header.split(" ")[1];
+
+        String isLogout = redisTemplate.opsForValue().get("blacklist:" + token);
+        if (isLogout != null) {
+            filterChain.doFilter(request, response);
+            throw new BusinessException(ErrorCode.UNAUTHORIZED);
+        }
 
         // 토큰 유효확인, 안에 담긴 User id가져옴
         // DB 유저 찾기
--- src/main/java/kongju/pickmeal/core/service/JwtService.java
+++ src/main/java/kongju/pickmeal/core/service/JwtService.java
@@ -10,7 +10,8 @@ import java.util.Optional;
 public interface JwtService {
     String createAccessToken(User user);
     String createRefreshToken(User user);
-
+    // 남은 만료시간 계산
+    Long getExpiration(String token);
     Optional<String> getSubFromAccessToken(String token) ;
     Optional<String> getSubFromRefreshToken(String token) ;
 
