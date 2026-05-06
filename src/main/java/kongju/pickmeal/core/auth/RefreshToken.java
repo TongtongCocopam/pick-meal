c198144cbd8d9a076d1d1c7adbdbdc93d8951c8c
jihyun <rlawlgus4980@gmail.com>
2026-05-06 20:02:44 +0900

    test : 컴포넌트 제거 후 수동으로 주입 사용

--- src/main/java/kongju/pickmeal/core/auth/RefreshToken.java
+++ src/main/java/kongju/pickmeal/core/auth/RefreshToken.java
@@ -1,14 +1,13 @@
 package kongju.pickmeal.core.auth;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
+
 import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
-@Entity
 @RedisHash(value = "refreshToken")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
--- src/main/java/kongju/pickmeal/infrastructure/config/SecurityConfig.java
+++ src/main/java/kongju/pickmeal/infrastructure/config/SecurityConfig.java
@@ -1,25 +1,43 @@
 package kongju.pickmeal.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import kongju.pickmeal.core.service.JwtService;
+import kongju.pickmeal.core.user.UserRepository;
+import kongju.pickmeal.api.security.JwtTokenFilter;
+
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter(jwtService, userRepository, redisTemplate);
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenFilter jwtTokenFilter) throws Exception {
         http
                 // CSRF설정 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
@@ -41,7 +59,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
