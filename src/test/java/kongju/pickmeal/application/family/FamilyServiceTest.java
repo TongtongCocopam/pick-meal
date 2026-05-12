7606ca845b62693f21f117ed91efc70d0123bb77
jihyun <rlawlgus4980@gmail.com>
2026-05-12 16:11:37 +0900

    test : 가족 합류 신청 성공케이스

--- src/test/java/kongju/pickmeal/application/family/FamilyServiceTest.java
+++ src/test/java/kongju/pickmeal/application/family/FamilyServiceTest.java
@@ -1,39 +1,44 @@
 package kongju.pickmeal.application.family;
 
+import java.util.Optional;
+
+import kongju.pickmeal.core.family.*;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
 import org.junit.jupiter.api.DisplayName;
-import static org.mockito.ArgumentMatchers.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.*;
+
 import kongju.pickmeal.core.user.User;
-import kongju.pickmeal.core.family.Family;
 import kongju.pickmeal.common.exception.ErrorCode;
-import kongju.pickmeal.core.family.FamilyRepository;
 import kongju.pickmeal.common.exception.BusinessException;
 import kongju.pickmeal.application.family.data.FamiliesRequest;
 import kongju.pickmeal.application.family.data.FamiliesResponse;
-
-import java.util.Optional;
 
 
 @ExtendWith(SpringExtension.class)
 public class FamilyServiceTest {
     @Mock
     private FamilyRepository familyRepository;
+    @Mock
+    private FamilyApplyRepository familyApplyRepository;
 
     @InjectMocks
     private FamilyService familyService;
+
+    public User createUser() {
+        return User.builder()
+                .loginId("testUser")
+                .build();
+    }
 
     @Nested
     @DisplayName("가족 그룹 생성 테스트")
@@ -45,9 +50,7 @@ public class FamilyServiceTest {
                     .familyName("고양이")
                     .build();
 
-            User user = User.builder()
-                    .loginId("testUser")
-                    .build();
+            User user = createUser();
 
             user.joinFamilyLeader(100L);
 
@@ -65,9 +68,7 @@ public class FamilyServiceTest {
                     .familyName("고양이")
                     .build();
 
-            User user = User.builder()
-                    .loginId("testUser")
-                    .build();
+            User user = createUser();
 
             FamiliesResponse.Create response = familyService.createFamily(request, user);
             assertEquals(request.familyName(), response.familyName());
@@ -78,7 +79,7 @@ public class FamilyServiceTest {
 
     @Nested
     @DisplayName("가족 합류 신청")
-    class FamilyApply{
+    class FamilyApply {
         @Test
         @DisplayName("이미 가족이 있을 경우")
         public void should_fail_apply_already_exist_family() {
@@ -86,9 +87,7 @@ public class FamilyServiceTest {
                     .invitationCode("초대코드라는뜻")
                     .build();
 
-            User user = User.builder()
-                    .loginId("testUser")
-                    .build();
+            User user = createUser();
 
             user.joinFamilyMember(12L);
 
@@ -100,27 +99,63 @@ public class FamilyServiceTest {
         }
 
         @Test
-        @DisplayName("잘못된 초대 코드일 경우")
-        public void should_fail_apply_unavailable_invitation_code(){
-
-        }
-
-        @Test
         @DisplayName("초대 코드를 찾지 못한 경우")
-        public void should_fail_apply_invitation_code_not_found(){
+        public void should_fail_apply_invitation_code_not_found() {
+            FamiliesRequest.Apply request = FamiliesRequest.Apply.builder()
+                    .build();
 
+            User user = createUser();
+
+            given(familyRepository.findByInvitationCode(anyString())).willReturn(Optional.empty());
+
+            BusinessException exception = assertThrows(BusinessException.class, () -> {
+                familyService.apply(request, user);
+            });
+
+            assertEquals(ErrorCode.INVALID_INVITATION_CODE, exception.getErrorCode());
         }
 
         @Test
         @DisplayName("이미 신청한 경우")
-        public void should_fail_apply_already_exists(){
+        public void should_fail_apply_already_exists() {
+            FamiliesRequest.Apply request = FamiliesRequest.Apply.builder()
+                    .invitationCode("초대코드라는뜻")
+                    .build();
 
+            User user = createUser();
+
+            Family family = Family.builder()
+                    .build();
+
+            given(familyRepository.findByInvitationCode(anyString())).willReturn(Optional.of(family));
+            given(familyApplyRepository.checkPendingApply(eq(user.getId()), eq(family.getId()), eq(ApplyStatus.PENDING))).willReturn(true);
+
+            BusinessException exception = assertThrows(BusinessException.class, () -> {
+                familyService.apply(request, user);
+            });
+
+            assertEquals(ErrorCode.ALREADY_PROCESSED, exception.getErrorCode());
         }
 
         @Test
         @DisplayName("성공 케이스")
-        public void should_success_apply(){
+        public void should_success_apply() {
+            FamiliesRequest.Apply request = FamiliesRequest.Apply.builder()
+                    .invitationCode("초대코드라는뜻")
+                    .build();
 
+            User user = createUser();
+
+            Family family = Family.builder()
+                    .build();
+
+            given(familyRepository.findByInvitationCode(anyString())).willReturn(Optional.of(family));
+            given(familyApplyRepository.checkPendingApply(eq(user.getId()), eq(family.getId()), eq(ApplyStatus.PENDING))).willReturn(false);
+
+            // 오류 없이 실행되었는지 체크
+            assertDoesNotThrow(() -> familyService.apply(request, user));
+
+            verify(familyApplyRepository, times(1)).save(any(JoinApply.class));
         }
     }
 
