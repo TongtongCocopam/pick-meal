607b4b00c2da60c73d9827be77e7ca247f7f600a
jihyun <rlawlgus4980@gmail.com>
2026-05-14 21:15:31 +0900

    test : dto수정에 따른 코드 변경

--- src/test/java/kongju/pickmeal/api/auth/AuthControllerTest.java
+++ src/test/java/kongju/pickmeal/api/auth/AuthControllerTest.java
@@ -1,36 +1,31 @@
 package kongju.pickmeal.api.auth;
 
-
-import jakarta.servlet.http.Cookie;
-import kongju.pickmeal.api.security.JwtTokenFilter;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.DisplayName;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import kongju.pickmeal.core.service.JwtService;
 import kongju.pickmeal.core.user.UserRepository;
 import kongju.pickmeal.common.exception.ErrorCode;
 import kongju.pickmeal.application.auth.AuthService;
+import kongju.pickmeal.application.auth.data.AuthDto;
 import kongju.pickmeal.common.exception.BusinessException;
-import kongju.pickmeal.application.auth.data.request.AuthRequest;
-import kongju.pickmeal.application.auth.data.response.AuthResponse;
 
 
 @WebMvcTest(AuthController.class)
@@ -55,13 +50,13 @@ public class AuthControllerTest {
         @Test
         @DisplayName("아이디와 일치하는 유저를 찾지 못했거나 비밀번호가 일치하지 않을 경우 에러 처리")
         public void should_fail_loginId_not_found() throws Exception {
-            AuthRequest.Login request = AuthRequest.Login
+            AuthDto.LoginRequest request = AuthDto.LoginRequest
                     .builder()
                     .loginId("wrongId")
                     .password("password123")
                     .build();
 
-            given(authService.login(any(AuthRequest.Login.class))).willThrow(new BusinessException(ErrorCode.LOGIN_FAILED));
+            given(authService.login(any(AuthDto.LoginRequest.class))).willThrow(new BusinessException(ErrorCode.LOGIN_FAILED));
 
             mockMvc.perform(post("/api/v1/auth/login")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -71,21 +66,20 @@ public class AuthControllerTest {
                     .andExpect(jsonPath("$.error.message").exists());
         }
 
-
         @Test
         @DisplayName("로그인 성공한 케이스")
         public void should_success_login() throws Exception {
-            AuthRequest.Login request = AuthRequest.Login
+            AuthDto.LoginRequest request = AuthDto.LoginRequest
                     .builder()
                     .loginId("wrongId")
                     .password("password123")
                     .build();
 
-            AuthResponse.Token token = AuthResponse.Token.builder()
+            AuthDto.TokenPair token = AuthDto.TokenPair.builder()
                     .accessToken("access_token_test")
                     .build();
 
-            given(authService.login(any(AuthRequest.Login.class))).willReturn(token);
+            given(authService.login(any(AuthDto.LoginRequest.class))).willReturn(token);
 
             mockMvc.perform(post("/api/v1/auth/login")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -100,17 +94,15 @@ public class AuthControllerTest {
     @DisplayName("로그아웃 테스트")
     class Logout {
         @Test
-        @DisplayName("필수 파라미터 누락")
-        public void should_fail_logout_params_missing() throws Exception {
-            AuthRequest.Token request = AuthRequest.Token.builder()
-                    .accessToken("")
-                    .build();
-
+        @DisplayName("인증 실패")
+        public void should_fail_logout_unauthorized() throws Exception {
+            willThrow(new BusinessException(ErrorCode.UNAUTHORIZED))
+                    .given(authService)
+                    .logout(isNull(), anyString());
             mockMvc.perform(post("/api/v1/auth/logout")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
-                    .andExpect(status().isBadRequest())
-                    .andDo(print())
+                            .cookie(new Cookie("refreshToken", "refresh_token_test"))
+                    )
+                    .andExpect(status().isUnauthorized())
                     .andExpect(jsonPath("$.success").value(false));
 
         }
@@ -118,16 +110,11 @@ public class AuthControllerTest {
         @Test
         @DisplayName("로그아웃 성공한 케이스")
         public void should_success_logout() throws Exception {
-            AuthRequest.Token request = AuthRequest.Token
-                    .builder()
-                    .accessToken("access")
-                    .build();
-
             mockMvc.perform(post("/api/v1/auth/logout")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)))
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer access_token_test")
+                            .cookie(new Cookie("refreshToken", "refresh_token_test"))
+                    )
                     .andExpect(status().isOk())
-                    .andDo(print())
                     .andExpect(jsonPath("$.success").value(true));
         }
     }
@@ -138,47 +125,31 @@ public class AuthControllerTest {
         @Test
         @DisplayName("파라미터 누락")
         public void should_fail_refresh_params_missing() throws Exception {
-            AuthRequest.Token request = AuthRequest.Token
-                    .builder()
-                    .accessToken("")
-                    .build();
+            willThrow(new BusinessException(ErrorCode.UNAUTHORIZED))
+                    .given(authService)
+                    .refresh(null);
 
-            mockMvc.perform(post("/api/v1/auth/refresh")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request))
-                            .cookie(new Cookie("refreshToken", "old-rt")))
-                    .andExpect(status().isBadRequest())
-                    .andDo(print())
-                    .andExpect(jsonPath("$.success").value(false));
+            mockMvc.perform(post("/api/v1/auth/refresh"))
+                    .andExpect(status().isUnauthorized());
         }
 
         @Test
         @DisplayName("토큰 재발급 성공 케이스")
         public void should_success_refresh() throws Exception {
-            AuthRequest.Token request = AuthRequest.Token
-                    .builder()
-                    .accessToken("accessToken")
-                    .build();
-
             // 새로운 토큰 발급
-            AuthResponse.Token tokenSet = AuthResponse.Token
+            AuthDto.TokenPair tokenSet = AuthDto.TokenPair
                     .builder()
                     .accessToken("new-access-token")
                     .refreshToken("new-refresh-token")
                     .build();
 
-            given(authService.refresh(any(), anyString())).willReturn(tokenSet);
+            given(authService.refresh(anyString())).willReturn(tokenSet);
 
             mockMvc.perform(post("/api/v1/auth/refresh")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request))
                             .cookie(new Cookie("refreshToken", "old-rt")))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.success").value(true))
-                    .andExpect(jsonPath("$.data.accessToken").value("new-access-token"))
-                    .andExpect(header().exists(HttpHeaders.SET_COOKIE))
-                    .andExpect(header().string(HttpHeaders.SET_COOKIE, containsString("refreshToken=new-refresh-token")))
-                    .andExpect(header().string(HttpHeaders.SET_COOKIE, containsString("HttpOnly")));
+                    .andExpect(jsonPath("$.data.accessToken").value("new-access-token"));
         }
     }
 }
