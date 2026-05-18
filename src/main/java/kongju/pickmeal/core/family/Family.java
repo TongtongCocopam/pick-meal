bcbc18496afb3d62e282b005af9e8e5303443ebd
jihyun <rlawlgus4980@gmail.com>
2026-05-18 12:00:48 +0900

    feat : 초대코드 재발급 업데이트 변수 추가, 초대코드 갱신 코드 추가

--- src/main/java/kongju/pickmeal/core/family/Family.java
+++ src/main/java/kongju/pickmeal/core/family/Family.java
@@ -2,6 +2,8 @@ package kongju.pickmeal.core.family;
 
 import java.time.LocalDateTime;
 
+import kongju.pickmeal.common.exception.BusinessException;
+import kongju.pickmeal.common.exception.ErrorCode;
 import lombok.Getter;
 import lombok.Builder;
 import lombok.AccessLevel;
@@ -22,8 +24,11 @@ public class Family extends BaseTimeEntity {
     private String invitationCode;
     private Long pickCount = 0L;
     private Long leaderId;
+    private LocalDateTime invitationCodeUpdatedAt;
+
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
 
     @Builder
     public Family(String familyName, String invitationCode, Long leaderId) {
@@ -31,5 +36,18 @@ public class Family extends BaseTimeEntity {
         this.invitationCode = invitationCode;
         this.pickCount = 0L;
         this.leaderId = leaderId;
+    }
+
+    /**
+     * 초대코드 재발급 시간 텀 10분
+     * @param invitationCode 초대코드
+     */
+    public void reissueInvitationCode(String invitationCode) {
+        if (this.invitationCodeUpdatedAt != null &&
+                this.invitationCodeUpdatedAt.isAfter(LocalDateTime.now().minusMinutes(10))) {
+            throw new BusinessException(ErrorCode.INVITATION_CODE_REISSUE_TOO_FAST);
+        }
+        this.invitationCode = invitationCode;
+        this.invitationCodeUpdatedAt = LocalDateTime.now();
     }
 }
