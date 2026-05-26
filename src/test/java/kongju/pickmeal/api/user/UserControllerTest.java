445e6d83f0f94e2903606d5791c2f0da76d79cc3
jihyun <rlawlgus4980@gmail.com>
2026-05-26 17:56:34 +0900

    test : 유저 선호 재료 선택, 건강 정보 수정 컨트롤러, 서비스 테스트 코드 구현

--- src/test/java/kongju/pickmeal/api/user/UserControllerTest.java
+++ src/test/java/kongju/pickmeal/api/user/UserControllerTest.java
@@ -1,9 +1,17 @@
 package kongju.pickmeal.api.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import kongju.pickmeal.application.user.data.UserDietProfileDto;
+import kongju.pickmeal.common.exception.BusinessException;
+import kongju.pickmeal.common.exception.ErrorCode;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import static org.mockito.BDDMockito.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.http.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import kongju.pickmeal.application.user.UserService;
 import kongju.pickmeal.application.user.data.UserDto;
+
 import static kongju.pickmeal.fixture.MemberFixture.createRequest;
 
 @WebMvcTest(UserController.class)
@@ -33,57 +42,101 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    @DisplayName("아이디가 6자 미만이면 에러 반환")
-    public void should_fail_invalid_loginId() throws Exception {
-        UserDto.SignupRequest request = createRequest("test", "test0000!!", "test0000!!", "test@test.com", "test@test.com", "tester", LocalDate.now());
+    @Nested
+    @DisplayName("회원가입 테스트")
+    class Signup {
+        @Test
+        @DisplayName("아이디가 6자 미만이면 에러 반환")
+        public void should_fail_invalid_loginId() throws Exception {
+            UserDto.SignupRequest request = createRequest("test", "test0000!!", "test0000!!", "test@test.com", "test@test.com", "tester", LocalDate.now());
 
-        mockMvc.perform(post("/api/v1/users/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false))
-                .andExpect(jsonPath("$.error.message").exists())
-                .andExpect(jsonPath("$.error.detail").exists());
+            mockMvc.perform(post("/api/v1/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.error.message").exists())
+                    .andExpect(jsonPath("$.error.detail").exists());
 
-        // 서비스가 한번도 실행되지 않았음을 검증
-        verify(userService, never()).signup(any());
+            // 서비스가 한번도 실행되지 않았음을 검증
+            verify(userService, never()).signup(any());
+        }
+
+        @Test
+        @DisplayName("비밀번호 불일치")
+        public void should_fail_mismatch_password() throws Exception {
+            UserDto.SignupRequest request = createRequest("test", "test0000!!", "wrong!!", "test@test.com", "test@test.com", "tester", LocalDate.now());
+
+            mockMvc.perform(post("/api/v1/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.error.message").exists());
+
+            // 서비스가 한번도 실행되지 않았음을 검증
+            verify(userService, never()).signup(any());
+        }
+
+        @Test
+        @DisplayName("회원가입 성공")
+        public void should_success_signup() throws Exception {
+            UserDto.SignupRequest request = createRequest();
+
+            UserDto.SignupResponse mockResponse = UserDto.SignupResponse.builder()
+                    .userId(1L)
+                    .nickName("tester")
+                    .build();
+
+            given(userService.signup(request)).willReturn(mockResponse);
+
+            mockMvc.perform(post("/api/v1/users/signup")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.success").value(true))
+                    .andExpect(jsonPath("$.data.userId").value(1L))
+                    .andExpect(jsonPath("$.data.nickName").value("tester"));
+        }
     }
 
-    @Test
-    @DisplayName("비밀번호 불일치")
-    public void should_fail_mismatch_password() throws Exception {
-        UserDto.SignupRequest request = createRequest("test", "test0000!!", "wrong!!", "test@test.com", "test@test.com", "tester", LocalDate.now());
+    @Nested
+    @DisplayName("유저 건강, 선호 식품 정보 수정")
+    class DietProfile {
+        @Test
+        @DisplayName("파라미터 누락")
+        public void should_fail_diet_profile_when_param_is_empty() throws Exception {
+            UserDietProfileDto.UpdateRequest request = UserDietProfileDto.UpdateRequest.builder()
+                    .ingredientPreferences(null)
+                    .diseases(null)
+                    .build();
 
-        mockMvc.perform(post("/api/v1/users/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false))
-                .andExpect(jsonPath("$.error.message").exists());
+            doThrow(new BusinessException(ErrorCode.INVALID_INPUT, "변경할 데이터가 존재하지 않습니다."))
+                    .when(userService).updateDietProfile(any(), any());
 
-        // 서비스가 한번도 실행되지 않았음을 검증
-        verify(userService, never()).signup(any());
-    }
+            mockMvc.perform(patch("/api/v1/users/me/diet-profile")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.error.message").exists());
+        }
 
-    @Test
-    @DisplayName("회원가입 성공")
-    public void should_success_signup() throws Exception {
-        UserDto.SignupRequest request = createRequest();
+        @Test
+        @DisplayName("성공케이스")
+        public void should_success_diet_profile() throws Exception {
 
-        UserDto.SignupResponse mockResponse = UserDto.SignupResponse.builder()
-                .userId(1L)
-                .nickName("tester")
-                .build();
+            UserDietProfileDto.UpdateRequest request = UserDietProfileDto.UpdateRequest.builder()
+                    .ingredientPreferences(List.of())
+                    .diseases(List.of())
+                    .build();
 
-        given(userService.signup(request)).willReturn(mockResponse);
+            mockMvc.perform(patch("/api/v1/users/me/diet-profile")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.success").value(true));
+        }
 
-        mockMvc.perform(post("/api/v1/users/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.data.userId").value(1L))
-                .andExpect(jsonPath("$.data.nickName").value("tester"));
     }
 }
