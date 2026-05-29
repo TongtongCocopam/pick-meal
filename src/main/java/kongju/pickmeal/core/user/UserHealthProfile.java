0c83e8e298b649bdbb4a904c1d2dbaba369562a3
jihyun <rlawlgus4980@gmail.com>
2026-05-29 14:27:10 +0900

    fix : 유저 건강 정보 테이블 update추가, 키 몸무게는 BigDecimal로 변경

--- src/main/java/kongju/pickmeal/core/user/UserHealthProfile.java
+++ src/main/java/kongju/pickmeal/core/user/UserHealthProfile.java
@@ -1,5 +1,6 @@
 package kongju.pickmeal.core.user;
 
+import java.math.BigDecimal;
 
 import lombok.Builder;
 import lombok.AccessLevel;
@@ -9,23 +10,32 @@ import lombok.NoArgsConstructor;
 import kongju.pickmeal.core.user.type.Gender;
 import kongju.pickmeal.core.common.BaseEntity;
 
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserHealthProfile extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Gender gender;
-    private Double height;
-    private Double weight;
+    @Column(precision = 4, scale = 1)
+    private BigDecimal height;
+    @Column(precision = 4, scale = 1)
+    private BigDecimal weight;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Builder
-    public UserHealthProfile(Gender gender, Double height, Double weight, User user) {
+    public UserHealthProfile(Gender gender, BigDecimal height, BigDecimal weight, User user) {
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.user = user;
+    }
+
+    public void update(Gender gender, BigDecimal height, BigDecimal weight){
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
     }
 }
