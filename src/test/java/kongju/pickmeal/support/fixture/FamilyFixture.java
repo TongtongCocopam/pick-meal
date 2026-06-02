5b9b7b991ab74cdccb80b818ad9c400c021e069d
jihyun <rlawlgus4980@gmail.com>
2026-06-02 15:23:36 +0900

    test : 가족 id생성 fixture추가

--- src/test/java/kongju/pickmeal/support/fixture/FamilyFixture.java
+++ src/test/java/kongju/pickmeal/support/fixture/FamilyFixture.java
@@ -1,13 +1,23 @@
 package kongju.pickmeal.support.fixture;
 
 import kongju.pickmeal.core.family.Family;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class FamilyFixture {
     public static Family family(){
         return Family.builder()
                 .familyName("Family")
-                .leaderId(1L)
                 .invitationCode("1234asdf")
                 .build();
+    }
+
+    public static Family familyWithId(String familyName, Long id) {
+        Family family = Family.builder()
+                .familyName(familyName)
+                .invitationCode("1234asdf")
+                .build();
+
+        ReflectionTestUtils.setField(family, "id", id);
+        return family;
     }
 }
