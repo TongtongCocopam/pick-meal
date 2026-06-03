da818a9ab021e60d50857468306919ba8c198127
jihyun <rlawlgus4980@gmail.com>
2026-06-03 17:53:11 +0900

    test : 회원가입, 로그인 예외 케이스 추가

--- src/test/java/kongju/pickmeal/support/fixture/TestSecurityConfig.java
+++ src/test/java/kongju/pickmeal/support/fixture/TestSecurityConfig.java
@@ -1,12 +1,15 @@
 package kongju.pickmeal.support.fixture;
 
-import kongju.pickmeal.api.security.CustomAccessDeniedHandler;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import kongju.pickmeal.api.security.CustomAccessDeniedHandler;
+
 
 @TestConfiguration
 @EnableMethodSecurity // PreAuthorize사용
@@ -24,6 +27,8 @@ public class TestSecurityConfig {
                 )
                 // 인증 정보는 확인
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/signup").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
