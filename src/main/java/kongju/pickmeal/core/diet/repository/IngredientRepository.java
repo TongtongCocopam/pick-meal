01ca37f803acb7fb6ee2840973820a1e2e0bd19c
jihyun <rlawlgus4980@gmail.com>
2026-06-08 17:05:41 +0900

    feat : 재료 이름으로 객체 찾기

--- src/main/java/kongju/pickmeal/core/diet/repository/IngredientRepository.java
+++ src/main/java/kongju/pickmeal/core/diet/repository/IngredientRepository.java
@@ -1,8 +1,11 @@
 package kongju.pickmeal.core.diet.repository;
 
+import java.util.Optional;
+
 import kongju.pickmeal.core.diet.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Optional<Ingredient> findByName(String name);
 }
