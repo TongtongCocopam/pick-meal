fc0a2f9f9b7e4c2bfb493463710d26af79b74b35
jihyun <rlawlgus4980@gmail.com>
2026-06-08 17:03:43 +0900

    feat : 레시피 아이디로 메뉴 찾기 추가

--- src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
+++ src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
@@ -3,5 +3,8 @@ package kongju.pickmeal.core.diet.repository;
 import kongju.pickmeal.core.diet.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Optional<Menu> findByExternalRecipeId(Long externalRecipeId);
 }
