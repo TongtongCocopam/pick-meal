4f2a226da6ecd1eb2dde67638a09264fc0654222
jihyun <rlawlgus4980@gmail.com>
2026-06-09 14:17:01 +0900

    feat : 중복 확인 메서드 추가

--- src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
+++ src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
@@ -1,10 +1,12 @@
 package kongju.pickmeal.core.diet.repository;
 
+import java.util.Optional;
+
 import kongju.pickmeal.core.diet.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     Optional<Menu> findByExternalRecipeId(Long externalRecipeId);
+    boolean existsByExternalRecipeId(Long externalRecipeId);
 }
