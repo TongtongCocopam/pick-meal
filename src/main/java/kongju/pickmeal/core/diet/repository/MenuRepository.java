4c86d4d31523a9eca7d710c91e1e92f4b43148d0
jihyun <rlawlgus4980@gmail.com>
2026-06-10 13:38:53 +0900

    refactor : 레포지토리와 어댑터 분리

--- src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
+++ src/main/java/kongju/pickmeal/core/diet/repository/MenuRepository.java
@@ -1,12 +1,13 @@
 package kongju.pickmeal.core.diet.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import kongju.pickmeal.core.diet.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MenuRepository extends JpaRepository<Menu, Long> {
+public interface MenuRepository{
     Optional<Menu> findByExternalRecipeId(Long externalRecipeId);
     boolean existsByExternalRecipeId(Long externalRecipeId);
+    List<Menu> saveAll(List<Menu> menus);
 }
