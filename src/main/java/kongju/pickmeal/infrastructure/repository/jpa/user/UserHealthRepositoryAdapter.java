067b5996422adcee67389fd38c305ce5f660f800
jihyun <rlawlgus4980@gmail.com>
2026-06-10 13:39:02 +0900

    refactor : 레포지토리와 어댑터 분리

---
+++ src/main/java/kongju/pickmeal/infrastructure/repository/jpa/user/UserHealthRepositoryAdapter.java
@@ -0,0 +1,27 @@
+package kongju.pickmeal.infrastructure.repository.jpa.user;
+
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import kongju.pickmeal.core.user.User;
+import kongju.pickmeal.core.user.UserHealthProfile;
+import kongju.pickmeal.core.user.repository.UserHealthRepository;
+
+
+@Repository
+@RequiredArgsConstructor
+public class UserHealthRepositoryAdapter implements UserHealthRepository {
+    private final UserHealthJpaRepository userHealthJpaRepository;
+
+    @Override
+    public Optional<UserHealthProfile> findByUser(User user) {
+        return userHealthJpaRepository.findByUser(user);
+    }
+
+    @Override
+    public UserHealthProfile save(UserHealthProfile userHealth) {
+        return userHealthJpaRepository.save(userHealth);
+    }
+}
