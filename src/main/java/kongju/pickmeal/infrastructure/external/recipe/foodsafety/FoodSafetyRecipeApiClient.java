c4af0256c2b22bb5f631ae3e7ee8e5e993fda37d
jihyun <rlawlgus4980@gmail.com>
2026-06-11 15:29:41 +0900

    feat : 식약처 레시피 정보 api get

---
+++ src/main/java/kongju/pickmeal/infrastructure/external/recipe/foodsafety/FoodSafetyRecipeApiClient.java
@@ -0,0 +1,42 @@
+package kongju.pickmeal.infrastructure.external.recipe.foodsafety;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.client.RestClient;
+import kongju.pickmeal.common.exception.ErrorCode;
+import kongju.pickmeal.common.exception.BusinessException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import kongju.pickmeal.infrastructure.config.FoodSafetyRecipeApiProperties;
+import kongju.pickmeal.infrastructure.external.recipe.foodsafety.data.FoodSafetyRecipeInfoResponse;
+
+
+@Component
+@RequiredArgsConstructor
+public class FoodSafetyRecipeApiClient {
+    private final RestClient restClient;
+    private final FoodSafetyRecipeApiProperties properties;
+
+    public FoodSafetyRecipeInfoResponse fetchRecipe(int startIdx, int endIdx) {
+        String url = "%s/%s/%s/%s/%d/%d".formatted(
+                properties.getBaseUrl(),
+                properties.getApiKey(),
+                properties.getServiceId(),
+                properties.getType(),
+                startIdx,
+                endIdx
+        );
+        try {
+            return restClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .body(FoodSafetyRecipeInfoResponse.class);
+
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            throw new BusinessException(ErrorCode.EXTERNAL_API_ERROR);
+        }
+    }
+
+
+}
