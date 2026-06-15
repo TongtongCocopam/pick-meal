5d363c3525fea3ec2bd3e91d9ef29a6ce4d44dfa
jihyun <rlawlgus4980@gmail.com>
2026-06-15 11:59:24 +0900

    feat : 메뉴 상세 정보 불러오기 컨트롤러

--- src/main/java/kongju/pickmeal/api/menu/MenuController.java
+++ src/main/java/kongju/pickmeal/api/menu/MenuController.java
@@ -4,15 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import kongju.pickmeal.core.menu.type.DishType;
-import kongju.pickmeal.application.menu.MenuService;
 import kongju.pickmeal.core.menu.type.MenuCategory;
+import kongju.pickmeal.application.menu.MenuService;
+import kongju.pickmeal.application.menu.data.MenuDto;
 import kongju.pickmeal.common.ApiResponse.ApiResponse;
 import kongju.pickmeal.application.menu.data.MenuFilterDto;
 
@@ -24,7 +22,7 @@ public class MenuController {
     private final MenuService menuService;
 
     @GetMapping("/filter-options")
-    public ResponseEntity<ApiResponse<MenuFilterDto.MetadataResponse>> getMenuFilters(){
+    public ResponseEntity<ApiResponse<MenuFilterDto.MetadataResponse>> getMenuFilters() {
         MenuFilterDto.MetadataResponse response = menuService.getFilterMetadata();
 
         return ResponseEntity
@@ -32,14 +30,25 @@ public class MenuController {
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<MenuFilterDto.ListItemResponse>> getMenuItems(
+    public ResponseEntity<ApiResponse<MenuDto.ListItemResponse>> getMenuItems(
             @RequestParam(required = false) MenuCategory category,
             @RequestParam(required = false) DishType dishType,
             @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
-            ){
-        MenuFilterDto.ListItemResponse response = menuService.searchMenus(category, dishType, pageable);
+    ) {
+        MenuDto.ListItemResponse response = menuService.searchMenus(category, dishType, pageable);
 
         return ResponseEntity
                 .ok(ApiResponse.success(response));
     }
+
+    @GetMapping("/menus/{menuId}")
+    public ResponseEntity<ApiResponse<MenuDto.DetailResponse>> getMenuDetail(
+            @PathVariable Long menuId
+    ) {
+        MenuDto.DetailResponse response = menuService.detailMenu(menuId);
+
+        return ResponseEntity
+                .ok(ApiResponse.success(response));
+    }
+
 }
