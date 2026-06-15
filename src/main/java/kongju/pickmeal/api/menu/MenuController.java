28247b096fb8b7059d04503b1dd6f2bf28b61134
jihyun <rlawlgus4980@gmail.com>
2026-06-15 10:47:53 +0900

    feat : 메뉴 검색 리스트 get 구현

--- src/main/java/kongju/pickmeal/api/menu/MenuController.java
+++ src/main/java/kongju/pickmeal/api/menu/MenuController.java
@@ -1,13 +1,18 @@
 package kongju.pickmeal.api.menu;
 
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kongju.pickmeal.core.menu.type.DishType;
 import kongju.pickmeal.application.menu.MenuService;
+import kongju.pickmeal.core.menu.type.MenuCategory;
 import kongju.pickmeal.common.ApiResponse.ApiResponse;
 import kongju.pickmeal.application.menu.data.MenuFilterDto;
 
@@ -21,6 +26,18 @@ public class MenuController {
     @GetMapping("/filter-options")
     public ResponseEntity<ApiResponse<MenuFilterDto.MetadataResponse>> getMenuFilters(){
         MenuFilterDto.MetadataResponse response = menuService.getFilterMetadata();
+
+        return ResponseEntity
+                .ok(ApiResponse.success(response));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<MenuFilterDto.ListItemResponse>> getMenuItems(
+            @RequestParam(required = false) MenuCategory category,
+            @RequestParam(required = false) DishType dishType,
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
+            ){
+        MenuFilterDto.ListItemResponse response = menuService.searchMenus(category, dishType, pageable);
 
         return ResponseEntity
                 .ok(ApiResponse.success(response));
