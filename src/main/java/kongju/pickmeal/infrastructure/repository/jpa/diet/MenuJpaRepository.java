d3e4e4f1516486a57aaf19fb32db46b43e6158d0
jihyun <rlawlgus4980@gmail.com>
2026-06-15 10:48:11 +0900

    feat : 메뉴 검색 리스트 쿼리 구현

--- src/main/java/kongju/pickmeal/infrastructure/repository/jpa/diet/MenuJpaRepository.java
+++ src/main/java/kongju/pickmeal/infrastructure/repository/jpa/diet/MenuJpaRepository.java
@@ -2,11 +2,24 @@ package kongju.pickmeal.infrastructure.repository.jpa.diet;
 
 import java.util.Optional;
 
-import kongju.pickmeal.core.menu.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import kongju.pickmeal.core.menu.Menu;
+import kongju.pickmeal.core.menu.type.DishType;
+import kongju.pickmeal.core.menu.type.MenuCategory;
+import org.springframework.data.repository.query.Param;
 
 
 public interface MenuJpaRepository extends JpaRepository<Menu, Long> {
     Optional<Menu> findByExternalRecipeId(Long externalRecipeId);
     boolean existsByExternalRecipeId(Long externalRecipeId);
+    @Query("""
+                    select m from Menu m
+                    where(:category is null or m.category = :category)
+                    and (:dishType is null or m.dishType = :dishType)
+            """)
+    Page<Menu> searchByFilters(@Param("category") MenuCategory category, @Param("dishType") DishType dishType, Pageable pageable);
 }
