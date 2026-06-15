3707e1852b325b9c6c2edfed3c3926fbbf490a2b
jihyun <rlawlgus4980@gmail.com>
2026-06-15 11:59:32 +0900

    feat : 메뉴 상세 정보 불러오기 서비스

--- src/main/java/kongju/pickmeal/application/menu/MenuService.java
+++ src/main/java/kongju/pickmeal/application/menu/MenuService.java
@@ -3,6 +3,11 @@ package kongju.pickmeal.application.menu;
 import java.util.List;
 import java.util.Arrays;
 
+import kongju.pickmeal.common.exception.BusinessException;
+import kongju.pickmeal.common.exception.ErrorCode;
+import kongju.pickmeal.core.menu.Ingredient;
+import kongju.pickmeal.core.menu.MenuIngredient;
+import kongju.pickmeal.core.menu.repository.MenuIngredientRepository;
 import lombok.RequiredArgsConstructor;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
@@ -12,6 +17,7 @@ import org.springframework.data.domain.Pageable;
 import kongju.pickmeal.core.menu.Menu;
 import kongju.pickmeal.core.menu.type.DishType;
 import kongju.pickmeal.core.menu.type.MenuCategory;
+import kongju.pickmeal.application.menu.data.MenuDto;
 import kongju.pickmeal.application.menu.data.MenuFilterDto;
 import kongju.pickmeal.core.menu.repository.MenuRepository;
 
@@ -21,6 +27,7 @@ import kongju.pickmeal.core.menu.repository.MenuRepository;
 @RequiredArgsConstructor
 public class MenuService {
     private final MenuRepository menuRepository;
+    private final MenuIngredientRepository menuIngredientRepository;
 
     public MenuFilterDto.MetadataResponse getFilterMetadata() {
         List<MenuFilterDto.CategoryResponse> categories = Arrays.stream(MenuCategory.values())
@@ -46,14 +53,14 @@ public class MenuService {
     }
 
 
-    public MenuFilterDto.ListItemResponse searchMenus(
+    public MenuDto.ListItemResponse searchMenus(
             MenuCategory category, DishType dishType, Pageable pageable
     ) {
         // 카테고리나 디쉬 타입이 있다면 쿼리로 불러오기
         Page<Menu> menuPage = menuRepository.searchByFilters(category, dishType, pageable);
 
-        List<MenuFilterDto.MenuInfo> menuInfoList = menuPage.stream()
-                .map(menu -> MenuFilterDto.MenuInfo.builder()
+        List<MenuDto.MenuInfoResponse> menuInfoList = menuPage.stream()
+                .map(menu -> MenuDto.MenuInfoResponse.builder()
                         .menuId(menu.getId())
                         .menuName(menu.getMenuName())
                         .category(menu.getCategory())
@@ -63,15 +70,46 @@ public class MenuService {
                 )
                 .toList();
 
-        MenuFilterDto.PageInfo pageInfo = MenuFilterDto.PageInfo.builder()
+        MenuDto.PageInfoResponse pageInfo = MenuDto.PageInfoResponse.builder()
                 .currentPage(menuPage.getNumber() + 1)
                 .totalPages(menuPage.getTotalPages())
                 .totalElements(menuPage.getTotalElements())
                 .build();
 
-        return MenuFilterDto.ListItemResponse.builder()
+        return MenuDto.ListItemResponse.builder()
                 .content(menuInfoList)
                 .pageInfo(pageInfo)
+                .build();
+    }
+
+    public MenuDto.DetailResponse detailMenu(Long menuId) {
+        // id로 메뉴 찾기
+        Menu menu = menuRepository.findById(menuId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_MENU_ID));
+
+        List<MenuIngredient> menuIngredients = menuIngredientRepository.findAllByMenuWithIngredient(menu);
+
+        // 메뉴 재료 정보 제공
+        List<MenuDto.IngredientResponse> ingredients = menuIngredients.stream()
+                .map(menuIngredient -> MenuDto.IngredientResponse.builder()
+                        .ingredientName(menuIngredient.getIngredient().getName())
+                        .quantityText(menuIngredient.getQuantityText())
+                        .build()
+                )
+                .toList();
+
+        // 메뉴 영양 정보
+        return MenuDto.DetailResponse.builder()
+                .menuId(menu.getId())
+                .menuName(menu.getMenuName())
+                .category(menu.getCategory())
+                .dishType(menu.getDishType())
+                .kcal(menu.getKcal())
+                .carbs(menu.getCarbs())
+                .fat(menu.getFat())
+                .sodium(menu.getSodium())
+                .protein(menu.getProtein())
+                .ingredients(ingredients)
                 .build();
     }
 
