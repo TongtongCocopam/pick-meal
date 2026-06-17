9a9b7a9d67490a40b7aa6e0b0e20140d4ec51ca8
jihyun <rlawlgus4980@gmail.com>
2026-06-17 12:23:20 +0900

    feat : 유저의 메뉴 선택 테이블 생성

---
+++ src/main/java/kongju/pickmeal/core/diet/UserMenuPick.java
@@ -0,0 +1,40 @@
+package kongju.pickmeal.core.diet;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import kongju.pickmeal.core.user.User;
+import kongju.pickmeal.core.menu.Menu;
+import kongju.pickmeal.core.diet.type.MealType;
+import kongju.pickmeal.core.common.BaseTimeEntity;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserMenuPick extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    private LocalDateTime meal_date;
+
+    @Enumerated(EnumType.STRING)
+    private MealType meal_type;
+
+    @Builder
+    public UserMenuPick(User user, Menu menu) {
+        this.user = user;
+        this.menu = menu;
+    }
+}
