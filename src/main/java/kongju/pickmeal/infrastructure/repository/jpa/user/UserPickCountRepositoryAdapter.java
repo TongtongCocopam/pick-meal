8e2885ea4d53f48f400ace9ad4ed9e9359d66e7c
jihyun <rlawlgus4980@gmail.com>
2026-06-18 13:56:06 +0900

    feat : 선택권 분배 테이블 관련 레포지토리 생성

---
+++ src/main/java/kongju/pickmeal/infrastructure/repository/jpa/user/UserPickCountRepositoryAdapter.java
@@ -0,0 +1,27 @@
+package kongju.pickmeal.infrastructure.repository.jpa.user;
+
+import kongju.pickmeal.core.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import kongju.pickmeal.core.user.UserPickCount;
+import kongju.pickmeal.core.user.repository.UserPickCountRepository;
+
+import java.util.Optional;
+
+
+@Repository
+@RequiredArgsConstructor
+public class UserPickCountRepositoryAdapter implements UserPickCountRepository {
+    private final UserPickCountJpaRepository userPickCountJpaRepository;
+
+    @Override
+    public UserPickCount save(UserPickCount userPickCount) {
+        return userPickCountJpaRepository.save(userPickCount);
+    }
+
+    @Override
+    public Optional<UserPickCount> findByUser(User user) {
+        return userPickCountJpaRepository.findByUser(user);
+    }
+}
