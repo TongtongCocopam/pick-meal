eacb37b2663970e0ff292146699a16948493bc87
jihyun <rlawlgus4980@gmail.com>
2026-06-24 15:17:53 +0900

    fix : 비동기 ai구현을 위해 응답 dto는 생성 id와 상태를 반환하는 것으로 변경

--- src/main/java/kongju/pickmeal/infrastructure/external/ai/data/DietGenerationDto.java
+++ src/main/java/kongju/pickmeal/infrastructure/external/ai/data/DietGenerationDto.java
@@ -1,9 +1,13 @@
 package kongju.pickmeal.infrastructure.external.ai.data;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
+import java.util.UUID;
 import java.time.LocalDate;
+
+import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
+
+import kongju.pickmeal.core.diet.type.DietGenerationStatus;
+
 
 public class DietGenerationDto {
     @Builder
@@ -12,11 +16,13 @@ public class DietGenerationDto {
             LocalDate startDate,
             @NotNull
             Integer dailyMealCount
-    ){
+    ) {
     }
 
     @Builder
     public record GenerateResponse(
-
-    ){}
+            UUID generationId,
+            DietGenerationStatus status
+    ) {
+    }
 }
