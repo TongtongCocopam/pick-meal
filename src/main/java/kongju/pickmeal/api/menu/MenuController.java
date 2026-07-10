213c979cc804195672b1ba88706a3c62b14b3adb
jihyun <rlawlgus4980@gmail.com>
2026-07-10 13:08:17 +0900

    feat : 커스텀 메뉴 추가 기능 구현

--- src/main/java/kongju/pickmeal/api/menu/MenuController.java
+++ src/main/java/kongju/pickmeal/api/menu/MenuController.java
@@ -1,18 +1,24 @@
 package kongju.pickmeal.api.menu;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import kongju.pickmeal.core.menu.type.DishType;
 import kongju.pickmeal.core.menu.type.MenuCategory;
 import kongju.pickmeal.application.menu.MenuService;
-import kongju.pickmeal.application.menu.data.MenuDto;
 import kongju.pickmeal.common.ApiResponse.ApiResponse;
-import kongju.pickmeal.application.menu.data.MenuFilterDto;
+import kongju.pickmeal.api.security.CustomUserDetails;
+import kongju.pickmeal.application.menu.data.MenuDto.DetailResponse;
+import kongju.pickmeal.application.menu.data.MenuDto.ListItemResponse;
+import kongju.pickmeal.application.menu.data.MenuFilterDto.MetadataResponse;
+import kongju.pickmeal.application.menu.data.FamilyCustomMenuDto.CreateRequest;
 
 
 @RestController
@@ -22,34 +28,43 @@ public class MenuController {
     private final MenuService menuService;
 
     @GetMapping("/filter-options")
-    public ResponseEntity<ApiResponse<MenuFilterDto.MetadataResponse>> getMenuFilters() {
-        MenuFilterDto.MetadataResponse response = menuService.getFilterMetadata();
+    public ResponseEntity<ApiResponse<MetadataResponse>> getMenuFilters() {
+        MetadataResponse response = menuService.getFilterMetadata();
 
         return ResponseEntity
                 .ok(ApiResponse.success(response));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<MenuDto.ListItemResponse>> getMenuItems(
+    public ResponseEntity<ApiResponse<ListItemResponse>> getMenuItems(
             @RequestParam(required = false) MenuCategory category,
             @RequestParam(required = false) DishType dishType,
             @RequestParam(required = false) String keyword,
             @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
     ) {
-        MenuDto.ListItemResponse response = menuService.searchMenus(category, dishType, keyword, pageable);
+        ListItemResponse response = menuService.searchMenus(category, dishType, keyword, pageable);
 
         return ResponseEntity
                 .ok(ApiResponse.success(response));
     }
 
     @GetMapping("/{menuId}")
-    public ResponseEntity<ApiResponse<MenuDto.DetailResponse>> getMenuDetail(
+    public ResponseEntity<ApiResponse<DetailResponse>> getMenuDetail(
             @PathVariable Long menuId
     ) {
-        MenuDto.DetailResponse response = menuService.detailMenu(menuId);
+        DetailResponse response = menuService.detailMenu(menuId);
 
         return ResponseEntity
                 .ok(ApiResponse.success(response));
     }
 
+    @PostMapping("/custom")
+    @PreAuthorize("hasRole('LEADER')")
+    public ResponseEntity<ApiResponse<Void>> createCustomMenu(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody @Valid CreateRequest request
+    ) {
+        menuService.createMenu(userDetails.id(), request);
+        return ResponseEntity.ok(ApiResponse.success());
+    }
 }
