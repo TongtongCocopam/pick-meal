a362c0f55c43e3f191bcab197b521d64f6d1a6ec
jihyun <rlawlgus4980@gmail.com>
2026-07-20 12:30:19 +0900

    feat : ai api설정에 valid추가

--- src/main/java/kongju/pickmeal/infrastructure/config/FoodSafetyRecipeApiProperties.java
+++ src/main/java/kongju/pickmeal/infrastructure/config/FoodSafetyRecipeApiProperties.java
@@ -3,14 +3,22 @@ package kongju.pickmeal.infrastructure.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties(prefix = "external.recipe.food-safety")
 public class FoodSafetyRecipeApiProperties {
+    @NotEmpty
     private String baseUrl;
+    @NotEmpty
     private String apiKey;
+    @NotEmpty
     private String serviceId;
+    @NotEmpty
     private String type;
 }
