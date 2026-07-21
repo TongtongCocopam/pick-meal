4ad272bdeb184243ce7c6165af42955f71484222
jihyun <rlawlgus4980@gmail.com>
2026-07-21 14:37:05 +0900

    refactor : ai식단 생성 실패 시 실패 코드 추가

--- src/main/java/kongju/pickmeal/common/exception/ErrorCode.java
+++ src/main/java/kongju/pickmeal/common/exception/ErrorCode.java
@@ -61,8 +61,9 @@ public enum ErrorCode {
     TOO_MANY_SELECTIONS(HttpStatus.BAD_REQUEST, "선택권 개수를 초과하였습니다."),
 
     DIET_MENU_LOCKED(HttpStatus.BAD_REQUEST,  "사용자가 선택한 메뉴는 교체할 수 없습니다."),
-    NOT_A_CANDIDATE(HttpStatus.BAD_REQUEST, "이미 식단으로 확정된 메뉴입니다."),
+    MENU_PICK_ALREADY_USED(HttpStatus.BAD_REQUEST, "이미 식단으로 확정된 메뉴입니다."),
     AI_PROCESS_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AI 식단 생성에 실패했습니다."),
+    DIET_GENERATION_NOT_FOUND(HttpStatus.BAD_REQUEST, "AI 식단 생성 테이블을 찾을 수 없습니다."),
 //    AI_RESPONSE_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "AI 응답이 지연되고 있습니다."),
     AI_DATA_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "생성된 데이터가 유효하지 않습니다."),
     SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "서버 점검 중입니다."),
