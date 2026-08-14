e73a1822ce5e3ee6922161763342c5d5a1c86040
jihyun <rlawlgus4980@gmail.com>
2026-08-14 15:43:21 +0900

    fix : env파일을 읽고 허용 설정하도록 쿠키 설정 변경

---
+++ src/main/java/kongju/pickmeal/infrastructure/config/properties/CookieProperties.java
@@ -0,0 +1,10 @@
+package kongju.pickmeal.infrastructure.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "cookie")
+public record CookieProperties(
+        boolean secure
+) {
+}
