3c726f6942d239764a5ca8b7aa3b1b31fc6979bc
jihyun <rlawlgus4980@gmail.com>
2026-08-24 16:27:59 +0900

    fix : 안쓰는 Exception제거, try, catch에러 처리 추가

--- src/main/java/kongju/pickmeal/application/menu/importer/PublicDataRecipeImportRunner.java
+++ src/main/java/kongju/pickmeal/application/menu/importer/PublicDataRecipeImportRunner.java
@@ -1,14 +1,17 @@
 package kongju.pickmeal.application.menu.importer;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 
+import kongju.pickmeal.common.exception.ErrorCode;
+import kongju.pickmeal.common.exception.BusinessException;
 import kongju.pickmeal.infrastructure.external.recipe.publicdata.service.MenuImportService;
 import kongju.pickmeal.infrastructure.external.recipe.publicdata.service.IngredientImportService;
 
-
+@Slf4j
 @Profile("public-data-import")
 @Component
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class PublicDataRecipeImportRunner implements CommandLineRunner {
     private final IngredientImportService ingredientImportService;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         menuImportService.importMenus(1,500);
 
         int totalCount = 180330;
@@ -26,7 +29,12 @@ public class PublicDataRecipeImportRunner implements CommandLineRunner {
 
         for (int start = 1; start <= totalCount; start += pageSize) {
             int end = Math.min(start + pageSize - 1, totalCount);
-            ingredientImportService.importIngredients(start, end);
+            try {
+                ingredientImportService.importIngredients(start, end);
+            }catch (Exception e){
+                log.error("public data import 실패 : start={}, end={}", start, end, e);
+                throw new BusinessException(ErrorCode.EXTERNAL_API_ERROR);
+            }
         }
     }
 
